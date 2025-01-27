860f3f8c9c9171ec781ed9411e69cc40d646bdbf
FelipeYevi <felipe.yevi@gmail.com>
2025-01-27 02:02:33 -0300

    subida de desafio terminado

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -19,4 +19,47 @@ caja4.addEventListener("click", () => {
   caja4.style.backgroundColor = "black";
 });
 
+/*Crea una página junto a un script que guarde dentro de una variable global
+ un color dependiendo de la letra del teclado presionada */
 
+let color = "";
+const elemento = document.getElementById("key");
+
+document.addEventListener("keydown", (event) => {
+  if (event.key == "a") {
+    color = "pink";
+  } else if (event.key === "s") {
+    color = "orange";
+  } else if (event.key === "d") {
+    color = "skyblue";
+  } else {
+    return;
+  }
+  elemento.style.backgroundColor = color;
+});
+
+/* Siguiendo con la lógica del punto anterior, al presionar las teclas q, w o e se
+ deberá crear un div nuevo de las mismas dimensiones antes mencionadas
+ con los colores morado, gris y café respectivamente. */
+
+const crearDiv=(color)=>{
+  const contenedor=document.getElementById("contenedor")
+  const nuevoDiv=document.createElement("div")
+  nuevoDiv.style.width="200px"
+  nuevoDiv.style.height="200px"
+  nuevoDiv.style.backgroundColor=color
+  nuevoDiv.classList.add("color-div")
+  contenedor.appendChild(nuevoDiv)
+}
+document.addEventListener("keydown",(event)=>{
+  if(event.key==="q"){
+    crearDiv("purple")
+  }
+  else if (event.key==="w"){
+    crearDiv("gray")
+  }
+  else if (event.key==="e"){
+    crearDiv("brown")
+  }
+
+})
